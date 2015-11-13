461400503b4230008f8c199a81c9bbd79cd15421
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-13 18:21:52 +0300

    Add success message when order is created

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,19 @@
+from django.contrib import messages
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import OrderForm
 
-# Create your views here.
+
 def order_new(request):
-    form = OrderForm()
-    return render(request, 'orders.html', {'form': form})
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # Save the form data
+            form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Order Succesfully Saved')
+            return HttpResponseRedirect('/order/new')
+    else:
+        form = OrderForm()
+        return render(request, 'orders.html', {'form': form})
