e27f2e1ed2039520b6177cc23e3ec108720f83e5
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-13 14:17:22 +0300

    Add models to admin

--- farmer/admin.py
+++ farmer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Buyer, Crop, Farmer, Recommendation
+
+admin.site.register(Buyer)
+admin.site.register(Crop)
+admin.site.register(Farmer)
+admin.site.register(Recommendation)
