119ed586606811dc4c72b2c1915bb982ae38a999
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-13 15:28:52 +0300

    Register Order model in the admin

--- farmer/admin.py
+++ farmer/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 
-from .models import Buyer, Crop, Farmer, Recommendation
+from .models import Buyer, Crop, Farmer, Order, Recommendation
 
 admin.site.register(Buyer)
 admin.site.register(Crop)
 admin.site.register(Farmer)
+admin.site.register(Order)
 admin.site.register(Recommendation)
