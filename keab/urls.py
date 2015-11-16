3339e8f666e303717f80c45b2767f28ef942ab4b
Kevin Ndung'u <kevgathuku@gmail.com>
2015-11-16 09:25:13 +0300

    Enable simple registration backend
    No registration emails are sent
    When user accounts are created, they are active by default

--- keab/urls.py
+++ keab/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^order/new/$', views.order_new, name='order_new'),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
 
 if not settings.DEBUG:
